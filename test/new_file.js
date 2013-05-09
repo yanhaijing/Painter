0e9cdac1468524fe0e60ba258c89c77ca44b7887
yan <yan@localhost.localdomain>
2013-05-09 08:33:13 +0800

    tsttest

---
+++ test/new_file.js
@@ -0,0 +1,28 @@
+/**
+ * @author yan
+ */
+
+function test(){
+	
+}
+
+(function(){
+	var a = 1;
+	/*
+	 * a
+	 */
+	var b;
+	
+	var c = function(){
+		
+	};
+	
+	c.prototype = {
+		a:function(){
+			
+		},
+		b:function(){
+			
+		},
+	}
+}());
