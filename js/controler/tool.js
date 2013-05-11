a587de4d09326f80677477099b68fee8f7a3d1e3
yanhaijing <yanhaijing1234@gmial.com>
2013-05-11 21:51:23 +0800

    update tool
    1.修改index.html
    2.添加当前工具数据
    3.添加工具数据
    4.修改工具控制

--- js/controler/tool.js
+++ js/controler/tool.js
@@ -52,9 +52,23 @@
 		          $defaultAttributePanel = $attributePanels.filter('#tool-shape-attribute-panel');  
 		            
 		    $attributePanels.dialog({position:'right bottom', autoOpen: false});//初始化所有属性面板
-		    $defaultAttributePanel.dialog("open");//代开默认面板
+		    
+		    this.setCurrentAttributePanel($defaultAttributePanel);//设置所有面板
+		    this.openAttributePanel($defaultAttributePanel);//打开当前面板
 		},
 		
+		/**
+		 * 打开当前属性面板 
+		 * @method openCurrentAttributePanel
+		 * @param {$object} [$attributePanel=currentAttributePanel] 腰带开的面版对象
+		 */
+		openAttributePanel:function($attributePanel){
+		    var 
+                  $attributePanels = $('.tool-attribute-panel'); 
+		    $attributePanel = $attributePanel || this.getCurrentAttributePanel();
+		    $attributePanels.dialog('close');
+		    $attributePanel.dialog('open');
+		},
 		/**
 		 * 获取当前工具属性面板对象
 		 * @method getCurrentAttributePanel 
@@ -67,7 +81,17 @@
 		/**
 		 *  设置当前工具属性面板对象
 		 * @method setCurrentAttributePanel 
+		 * @param {$object} $attributePanel 要设置的属性面板
+		 * @return {Bollean} 是否设置成功
 		 */
+		setCurrentAttributePanel:function($attributePanel){
+		    if($attributePanel !== undefined){
+		        this.currentOpenAttributePanel = $attributePanel;
+		        return true;
+		    } 
+		    
+		    return false;
+		},
 		/**
 		 * 绑定事件
 		 * @event bindEvent
@@ -75,15 +99,17 @@
 		bindEvent:function(){
 			var 
 			     $document = $(document),
-			     $toolWrap = $('#tool-wrap');
+			     $toolWrap = $('#tool-wrap'),
+			     that = this;
 			     
 			//绑定工具点击事件，所有按钮的点击效果
 			$document.delegate('#tool-wrap .tool button', 'click', function(){
 			    var 
 			         $this = $(this),
 			         temp = $this.attr('data-tool-panel'),
-			         $toolPanel = $('.tool-panel .' + temp, $toolWrap),
-			         $currentToolPanel = $('.tool-panel .wrap:visible', $toolWrap);
+			         $toolPanel = $('.tool-panel .' + temp, $toolWrap),//点击要显示面板
+			         $currentToolPanel = $('.tool-panel .wrap:visible', $toolWrap),//当前可见工具面板
+			         $currentToolAttributePanel = $('#tool-' + temp + '-attribute-panel');//当前属性工具面板			         
 			    
 			    //为按钮添加点击效果     
 			    $('.tool button.active', $toolWrap).removeClass('active');
@@ -92,6 +118,10 @@
 			    //操作工具面板
 			    $currentToolPanel.hide();
 			    $toolPanel.show();
+			    
+			    //操作属性面板
+			    that.setCurrentAttributePanel($currentToolAttributePanel);//设置所有面板
+                that.openAttributePanel($currentToolAttributePanel);//打开当前面板
 			});
 		}
 	};
---
+++ js/model/toolModel.js
@@ -0,0 +1,120 @@
+/**
+ * @author yan
+ * @module tool
+ */
+
+(function($, global){
+	"use strict";
+	
+	var Tool,Line;
+	
+	/**
+	 * 工具对象
+	 * @class Tool
+	 * @constructor
+	 * @param {Object} option 当前工具参数
+	 * @extend Tool.prototype
+	 */
+	Tool = function(option){
+		this.name = 'tool';
+		this.option = {
+			strokeStyle:'#000',
+			fillStyle:'#000',
+			lineWidth:1
+		};
+		
+		//设置参数
+		option !== undefined ? $.extend(this.option, option) : 0;
+	};
+	
+	/**
+	 * 工具原型
+	 * @class Tool.prototype
+	 * @strict
+	 */
+	Tool.prototype = {		
+		/**
+		 * 获取名字
+		 * @method getName
+		 * @return {String} 当前的名字
+		 */
+		getName:function(){
+			return this.name;	
+		},
+		
+		/**
+		 * 设置工具参数对象
+		 * @method setOption
+		 * @param {Object} option 设置的参数集和
+		 * @return {Bollean} 是否设置成功
+		 */
+		setOption:function(option){
+			//检查入口参数是否是对象
+			if(typeof option === 'object'){
+				$.extend(this.option, option);
+				return true;
+			} 
+			
+			return false;
+		},
+		
+		/**
+		 * 获取工具参数对象
+		 * @method getOption 
+		 * @return {Object} 返回当前工具参数对象
+		 */
+		getOption:function(){
+			return this.option;
+		}
+	};
+	
+	/**
+	 * 工具对象
+	 * @class Line
+	 * @constructor
+	 * @extend Line.prototype
+	 */
+	Line = function(){
+	    /**
+	     * 名称
+	     * @property name
+	     * @type String
+	     * @defult 'line' 
+	     */
+		this.name = 'line';
+		
+		/**
+		 * 初始化
+		 * @method init 
+		 * @return {Bollean} 初始化是否成功
+		 */
+		this.init = function(){
+		    //获取当前属性
+		    var 
+		      $attributePanel = $('#tool-shape-attribute-panel'),
+		      width = $('.width',$attributePanel).eq(0).val(),
+		      opacity = $('.opacity',$attributePanel).eq(0).val(),
+		      color = $('#tool-wrap .tool .color').eq(0).val();	      
+			
+			//设置参数
+			return this.setOption({
+                lineWidth: width,
+                opacity: opacity,
+                strokeStyle: color
+            });
+		};
+	};
+	
+	/**
+	 * 直线工具原型
+	 * @class Line.prototype
+	 * @strict
+	 */
+	Line.prototype = new Tool();
+	
+	//添加工具到数据层
+	window.painter = window.painter || {};
+	window.painter.model = window.painter.model || {};
+	window.painter.model.toolModel = window.painter.model.toolModel || {};
+	window.painter.model.toolModel.Line = Line;
+}(jQuery, window));
