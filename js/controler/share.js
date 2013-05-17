01c8cc343a5ca86d3200b20e80470332aed21661
yanhaijing <yanhaijing1234@gmial.com>
2013-05-17 15:34:16 +0800

    添加分享到腾讯微博功能

--- js/controler/share.js
+++ js/controler/share.js
@@ -4,7 +4,7 @@
         url : location.href,
         showcount : '1', /*是否显示分享总数,显示：'1'，不显示：'0' */
         desc : '号外 号外 给大家推荐一款HTML5绘图程序', /*默认分享理由(可选)*/
-        summary : 'Painter是一款基于HTML5 Canvas绘图程序，能够运行在所有平台，非常强大，可以试试哦，亲，记得用支持html的浏览器哦', /*分享摘要(可选)*/
+        summary : 'Painter是一款基于HTML5 Canvas绘图程序，能够运行在所有平台，非常强大，可以试试哦，亲，记得用支持html5的浏览器哦', /*分享摘要(可选)*/
         title : 'Painter', /*分享标题(可选)*/
         site : 'https://github.com/yanhaijing', /*分享来源 如：腾讯网(可选)*/
         pics : 'http://yanhaijing.github.io/Painter/images/Painter.gif', /*分享图片的路径(可选)*/
@@ -16,5 +16,5 @@
     for (var i in p) {
         s.push(i + '=' + encodeURIComponent(p[i] || ''));
     }
-    $("#nav-share-qzone").attr("href", "http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?" + s.join('&'));
+    $("#nav-share-qzone > a").attr("href", "http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?" + s.join('&'));
 })(); 
