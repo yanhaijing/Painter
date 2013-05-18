3284e5dda345ff6062352f411ebb113beab37577
yanhaijing <yanhaijing1234@gmial.com>
2013-05-18 21:55:16 +0800

    add share
    添加社交功能

--- js/controler/share.js
+++ js/controler/share.js
@@ -18,3 +18,26 @@
     }
     $("#nav-share-qzone > a").attr("href", "http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?" + s.join('&'));
 })();
+
+
+(function() {
+    var p = {
+        url : location.href, /*获取URL，可加上来自分享到QQ标识，方便统计*/
+        desc : '号外 号外 给大家推荐一款HTML5绘图程序', /*分享理由(风格应模拟用户对话),支持多分享语随机展现（使用|分隔）*/
+        title : 'Painter', /*分享标题(可选)*/
+        summary : 'Painter是一款基于HTML5 Canvas绘图程序，能够运行在所有平台，非常强大，可以试试哦，亲，记得用支持html5的浏览器哦', /*分享摘要(可选)*/
+        pics : 'http://yanhaijing.github.io/Painter/images/Painter.gif', /*分享图片(可选)*/
+        flash : '', /*视频地址(可选)*/
+        site : 'http://yanhaijing.github.io/Painter/', /*分享来源(可选) 如：QQ分享*/
+        style : '101',
+        width : 96,
+        height : 24
+    };
+    var s = [];
+    for (var i in p) {
+        s.push(i + '=' + encodeURIComponent(p[i] || ''));
+    }
+    $("#nav-share-qq > a").attr("href", "http://connect.qq.com/widget/shareqq/index.html?" + s.join('&'));
+    //document.write(['<a class="qcShareQQDiv" href="http://connect.qq.com/widget/shareqq/index.html?', s.join('&'), '" target="_blank">分享到QQ</a>'].join(''));
+})(); 
+
