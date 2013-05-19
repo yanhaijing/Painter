a135d345e2d18b5336ecf0e55b9a918f4dfbdb74
yanhaijing <yanhaijing1234@gmial.com>
2013-05-19 14:38:26 +0800

    完成加载功能
    1.添加载入功能

---
+++ js/controler/loading.js
@@ -0,0 +1,10 @@
+/**
+ * @author yan
+ */
+
+(function($, global){
+    "use strict";
+    var load = 1;
+    global.console.log("load",load++);
+    $("#loading").fadeOut();    
+}(jQuery, window));
