c5d419d5061f7ee479b22c262b487eec06e17804
颜海镜 <yanhaijing@yeah.net>
2013-09-25 15:40:48 +0800

    添加我的微博
    1.添加我的微博
    2.将百度统计改为异步

---
+++ js/baidu.js
@@ -0,0 +1,11 @@
+/**
+ * @author yanhaijing
+ */
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?53ee8889aa158cbb581bdbc9a373b5f0";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
